35369435c55bd0257d2e2aca1693a835b096300e
jax <jjackson17mv@gmail.com>
2019-07-23 16:43:12 -0700

    k

--- google-search/server/server.js
+++ google-search/server/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 6000;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
